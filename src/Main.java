5a4930a747db0e1dcc27ad909594ea45179d2f14
312 <ermekovanuraiym00@icloud.com>
2026-01-28 17:50:07 +0600

    Car

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,34 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.fio = "Ularbekov Shaamidin";
-        person1.age = 16;
-        person1.gender = "Male";
-        person1.nation = "Kyrgyz";
-        person1.proffession = "Java developer";
-        person1.holostoi = true;
+    Car car1 = new Car();
+    car1.model = "Ford";
+    car1.color = "Red";
+    car1.year = 2021;
 
-        System.out.println("FIO:" + person1.fio);
-        System.out.println("Age:" + person1.age);
-        System.out.println("Gender:" + person1.gender);
-        System.out.println("Nation:" + person1.nation);
-        System.out.println("Prof:" + person1.proffession);
-        System.out.println("Holostoi:" + person1.holostoi);
+    System.out.println(car1.model);
+    System.out.println(car1.color);
+    System.out.println(car1.year);
+
+    Car car2 = new Car();
+    car2.model = "Toyota";
+    car2.color = "Blue";
+    car2.year = 2001;
+
+    System.out.println(car2.model);
+    System.out.println(car2.color);
+    System.out.println(car2.year);
+
+    Car car3 = new Car();
+    car3.model = "Honda";
+    car3.color = "Red";
+    car3.year = 1991;
+
+    System.out.println(car3.model);
+    System.out.println(car3.color);
+    System.out.println(car3.year);
 
 
-        System.out.println(person1.run());
-        System.out.println(person1.speak());
-        System.out.println(person1.getBirthDate());
-
-
-        }
     }
-}
+    }
+
